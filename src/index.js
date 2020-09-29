c842611d5b5a5305278be563942cfc33682e8d0b
gribanovartem <gribanovartem22@gmail.com>
2020-09-29 21:12:22 +0300

    feat: implement 'towel-sort'

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const arr = [];
+  let forwardWay = true;
+  if(matrix) {
+    matrix.forEach(item => {
+      if(forwardWay) {
+        item.forEach(item1 => {
+          arr.push(item1);
+        });
+        forwardWay = false;;
+      } else {
+        item.reverse().forEach(item1 => {
+          arr.push(item1);
+        });
+        forwardWay = true;
+      }
+    });
+  }
+  return arr;
 }
